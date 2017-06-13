f9062f7cd4981106ef41e0312296f655b782857b
zoolsher <zoolsher@gmail.com>
2017-06-13 15:33:19 +0800

    file name

--- src/Resource.js
+++ src/Resource.js
@@ -2,7 +2,7 @@ var RESOURCES = {
 
 // images,
   "s_Tiny.js_png": "res/images/Tiny.js.png",
-  "all.png": "res/images/2WEhf.png",
+  "all.png": "res/images/2WEhF.png",
   "s_font_fnt": "res/fonts/font.fnt",
   "s_font_png": "res/fonts/font.png"
 };
